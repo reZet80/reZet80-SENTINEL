7cbc1769487bd540e6766ddac9d2d74cb223be7f
Adrian H. Hilgarth <59913016+reZet80@users.noreply.github.com>
2026-08-22 15:33:51 +0200

    Update lcd.asm

--- SENTINEL.R1/src/lcd.asm
+++ SENTINEL.R1/src/lcd.asm
@@ -1,1 +1,11 @@
-
+;=============================================================================
+; SENTINEL LCD configuration (lcd.asm) [last modified: 2026-07-26]
+; (c) copyright 2016-2026 Adrian H. Hilgarth (all rights reserved)
+; reZet80 - Z80-based retrocomputing and retrogaming (rezet80.blogspot.com)
+; indentation setting: tab size = 8
+;=============================================================================
+; LCD configuration:
+_LCD_40x1:	equ 1			; use 40x1 LCD
+_LCD_16x2:	equ 0			; use 16x2 LCD
+_LCD_16x1:	equ 0			; use 16x1 LCD
+;=============================================================================
